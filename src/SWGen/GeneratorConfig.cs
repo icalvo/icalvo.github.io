3f09cc2b6ccbabf440d83dd6b526a8f2f4ab3e3f
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2024-03-01 12:04:04 +0100

    Logging enhancements

--- src/SWGen/GeneratorConfig.cs
+++ src/SWGen/GeneratorConfig.cs
@@ -1,5 +1,29 @@
-﻿namespace SWGen;
+﻿using System.Runtime.CompilerServices;
 
-public record GeneratorConfig(Generator Generator, Func<AbsolutePathEx, RelativePathEx, bool> MatchesFile)
+namespace SWGen;
+
+public record GeneratorConfig
 {
+    private readonly string? _matcherName;
+
+    public GeneratorConfig(Generator generator, Func<AbsolutePathEx, RelativePathEx, bool> matchesFile, [CallerArgumentExpression(nameof(matchesFile))] string? argumentName = null)
+    {
+        _matcherName = argumentName ?? "unknown";
+        Generator = generator;
+        MatchesFile = matchesFile;
+    }
+
+    public Generator Generator { get; init; }
+    public Func<AbsolutePathEx, RelativePathEx, bool> MatchesFile { get; init; }
+
+    public override string ToString()
+    {
+        return $"GeneratorConfig {{ Generator = {Generator.GetType().Name}, Matcher = {_matcherName} }}";
+    }
+
+    public void Deconstruct(out Generator generator, out Func<AbsolutePathEx, RelativePathEx, bool> matchesFile)
+    {
+        generator = Generator;
+        matchesFile = MatchesFile;
+    }
 }
