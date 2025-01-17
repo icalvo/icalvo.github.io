df82a7fe61d777ca3f2b0bc8ff45c1676512c332
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2025-01-17 12:05:53 +0100

    Added comments explaining why we use `ICreatable<T>`.

--- src/SWGen/ICreatable.cs
+++ src/SWGen/ICreatable.cs
@@ -1,6 +1,20 @@
 ﻿namespace SWGen;
 
-public interface ICreatable<T>
+/// <summary>
+/// Object that can be created without parameters.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <remarks>
+/// This looks a lot like "objects with default constructor", so why use this interface instead?
+///
+/// This is used for document metadata. We want to be able to create default metadata values.
+/// Why not simply use a new() constraint? The problem is that when you have a default constructor then
+/// you cannot have a `required` member, and at the same time build a new instance with T() because
+/// `required` means you must use the object initializer.
+///
+/// Since we want to promote the object initializer syntax for metadata we used this interface. 
+/// </remarks>
+public interface ICreatable<out T>
 {
     static abstract T Create();
 }
